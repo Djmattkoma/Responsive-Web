87bb92dc46823aae640cfb3ddc986c7c5549d6c4
Djmattkoma <lmateka1999@gmail.com>
2021-11-30 19:32:07 +0100

    first commit

---
+++ script/animaton.js
@@ -0,0 +1,7 @@
+function Menuup(){
+    document.getElementById("menu-1").classList.toggle("active");
+  }
+
+  /*function Socmenuup(){
+    document.getElementById("socmedia").classList.toggle("active");
+  }*/
